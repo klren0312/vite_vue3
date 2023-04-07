af26f1cd943baf015b4e756526a0da7f82e35350
klkl <kuanglong0312@sina.com>
2023-04-07 17:07:40 +0800

    feat: 路由注册

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { setupRouter } from './routers'
 
 const app = createApp(App)
-
+setupRouter(app)
 app.use(createPinia())
 
 app.mount('#app')
